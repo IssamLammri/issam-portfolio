cc659b6640ac94afe98704f53b5020f838ea4eb4
IssamLammri <issamlamri34000@gmail.com>
2023-07-22 15:19:28 +0200

    🎨 cut out the pages

--- public/js/jquery.easing.1.3.js
+++ public/js/jquery.easing.1.3.js
@@ -7,9 +7,7 @@
  * TERMS OF USE - jQuery Easing
  * 
  * Open source under the BSD License. 
- * 
- * Copyright Ã‚Â© 2008 George McGinley Smith
- * All rights reserved.
+ *
  * 
  * Redistribution and use in source and binary forms, with or without modification, 
  * are permitted provided that the following conditions are met:
@@ -176,10 +174,7 @@ jQuery.extend( jQuery.easing,
  * TERMS OF USE - EASING EQUATIONS
  * 
  * Open source under the BSD License. 
- * 
- * Copyright Ã‚Â© 2001 Robert Penner
- * All rights reserved.
- * 
+ *
  * Redistribution and use in source and binary forms, with or without modification, 
  * are permitted provided that the following conditions are met:
  * 
